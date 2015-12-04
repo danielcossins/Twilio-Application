0627881891195a0e5f028aef1b4f0041b28d8ae0
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-04 14:50:31 -0600

    texting working

---
+++ TwilioApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Twilio;
+
+namespace TwilioApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Find your Account Sid and Auth Token at twilio.com/user/account 
+            string AccountSid = "AC85f12f79e35f3ea3f1fb985df2ee99a9";
+            string AuthToken = "ef3e7e352e393b144f8b74a2ed6c24af";
+            var twilio = new TwilioRestClient(AccountSid, AuthToken);
+
+            var message = twilio.SendMessage("+18017841260", "9132169521", "asdfsadfasdf", "");
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
