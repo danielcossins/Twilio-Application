b95efe15bbfd6716044e0492adb0c547c2968722
danielcossins11 <daniel.cossins@yahoo.com>
2015-12-05 14:15:29 -0600

    simple console interface for texting Daniel

--- TwilioApp/Program.cs
+++ TwilioApp/Program.cs
@@ -16,8 +16,28 @@ namespace TwilioApp
             string AuthToken = "ef3e7e352e393b144f8b74a2ed6c24af";
             var twilio = new TwilioRestClient(AccountSid, AuthToken);
 
-            var message = twilio.SendMessage("+18017841260", "9132169521", "asdfsadfasdf", "");
-            Console.WriteLine(message.Sid);
+            //var message = twilio.SendMessage("+18017841260", "9132169521", "asdfsadfasdf", "");
+            //Console.WriteLine(message.Sid);
+            Console.WriteLine("Welcome to the text Daniel Application!");
+            Console.WriteLine("Enter [exit] at anytime to end the program.");
+            string prompt = "--> ";
+            Console.Write(prompt);
+            string input = Console.ReadLine();
+            while (true)
+            {
+                if (input != "exit")
+                {
+                    var message = twilio.SendMessage("+18017841260", "9132169521", input, "");
+                    //Console.WriteLine(message.Sid);
+                    Console.WriteLine("Enter another message");
+                    Console.Write(prompt);
+                    input = Console.ReadLine();
+                }
+                else
+                {
+                    break;
+                }
+            }
         }
     }
 }
